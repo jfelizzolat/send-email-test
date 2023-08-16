3078be45615022bf54c41cd95b655fd5565f8d6b
Jeffrey Felizzola <jfelizzolat@gmail.com>
2023-08-15 21:12:13 -0300

    Prueba de actualizacion

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function enviarCorreo(datos = { Para: '', Logo: '' }) {
         $("#Progress").hide();
         swal(
           "Alerta!",
-          "El formulario no se diligenció correctamente. Por favor verifique los campos resaltados e intente de nuevo",
+          "El formulario no se diligenció correctamente.\nPor favor verifique los campos resaltados e intente de nuevo",
           "warning"
         );
       },
@@ -58,7 +58,7 @@ function enviarCorreo(datos = { Para: '', Logo: '' }) {
     Swal.fire(
       {
         title: '¡Alerta!',
-        text: 'El formulario no se diligenció correctamente. Por favor verifique los campos resaltados e intente de nuevo',
+        text: 'El formulario no se diligenció correctamente.\nPor favor verifique los campos resaltados e intente de nuevo',
         icon: 'warning',
         confirmButtonText: 'Ok'
       }
